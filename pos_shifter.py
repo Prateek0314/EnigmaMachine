632e1a0bd4ae23bd9ea01452502b8da594cf5046
Prateek <89270308+Prateek0314@users.noreply.github.com>
2022-12-30 14:31:19 -0700

    Add files via upload

---
+++ pos_shifter.py
@@ -0,0 +1,25 @@
+
+def shifter1(p1):
+    if p1==25:
+        p1=0
+
+    p1+=1
+    return p1
+
+def shifter2(p1,p2):
+    if p1==20:
+        if p2==25:
+            p2=0
+
+        p2+=1
+    return p2
+
+def shifter3(p2,p3):
+    if p2==4:
+        if p3==25:
+            p3=0
+
+        p3+=1
+    return p3
+    
+    
